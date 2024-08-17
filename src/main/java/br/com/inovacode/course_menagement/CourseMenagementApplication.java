54d0d93fdff3725779679262a6ab22470c974388
Marcos Silva <marcossilva@Marcoss-Notebook.local>
2024-08-17 11:08:12 -0400

    create the application

---
+++ src/main/java/br/com/inovacode/course_menagement/CourseMenagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.inovacode.course_menagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseMenagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseMenagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/inovacode/course_menagement/CourseMenagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.inovacode.course_menagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseMenagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
